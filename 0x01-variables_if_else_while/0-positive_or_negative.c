52352d0c4fb163276d2b91f73f887bfe6765001d
MaryVeraUcheze <uchezemaryvera@gmail.com>
2023-02-17 13:34:02 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,11 +1,12 @@
-#include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
 /**
-*main-Entry point
+*main-A program The output of the program should be:
+*The number, followed by
 *if the number is greater than 0: is positive
 *if the number is 0: is zero
 *if the number is less than 0: is negative
+*followed by a new line
 *
 *Return:Always 0 (Success)
 */
@@ -14,6 +15,7 @@ int main(void)
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
+/* your code goes there */
 if (n > 0)
 {
 	printf("%d is positive\n", n);
