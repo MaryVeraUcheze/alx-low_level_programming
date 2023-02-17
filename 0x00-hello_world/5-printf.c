00f1af258012ee01a925f0d8a87abebe5decbfce
MaryVeraUcheze <uchezemaryvera@gmail.com>
2023-02-16 16:23:24 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-/*
-*main - A program that prints a line using printf function
+/**
+*main-A program with print function
 *
-*Return: Always 0 (Success)
+*Return:Always 0 (Success)
 */
 int main(void)
 {
