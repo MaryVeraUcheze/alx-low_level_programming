3ab8621d1c835edc79fbfdd8a7a35ca6dc053780
MaryVeraUcheze <uchezemaryvera@gmail.com>
2023-02-27 17:20:55 -0800

    7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,20 +9,20 @@
 
 void puts_half(char *str)
 {
-	int len = strlen(str);
-	int start, i;
+int len = strlen(str);
+int start, i;
 
-	if (len % 2 == 0)
-	{
-	start = len / 2;
-	}
-	else
-	{
-	start = (len - 1) / 2;
-	}
-	for (i = start; i < len; i++)
-	{
-	printf("%c", str[i]);
-	}
-	printf("\n");
+if (len % 2 == 0)
+{
+start = len / 2;
+}
+else
+{
+start = (len - 1) / 2;
+}
+for (i = start; i < len; i++)
+{
+printf("%c", str[i]);
+}
+printf("\n");
 }
