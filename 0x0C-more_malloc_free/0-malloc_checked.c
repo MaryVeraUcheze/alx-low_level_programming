92bda1e30f637c799832d9be45887024d1034be2
Mary-Vera <uchezemaryvera@gmail.com>
2023-03-16 13:30:10 -0700

    initial commit

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 
 /**
- * malloc_checked -  a function that allocates memory
- * @b: size
- * return: a pointer to allocated memory
+ * malloc_checked - a function that allocates memory
+ * @b: size of memeory
+ * Return: a pointer to allocated memory
 */
 
 void *malloc_checked(unsigned int b)
