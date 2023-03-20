2e5e08c5151d683bbdb28b9d66aa5839090a4ec4
MaryVeraUcheze <uchezemaryvera@gmail.com>
2023-03-20 15:01:17 -0700

    1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -8,12 +8,15 @@
  * @age: age to initialize
  * @owner: owner to initialize
  */
-void init_dog(struct dog *d, char *name, float age, char *owner)
+void init_dog(struct dog *d, char *name, float age, char *owner);
 {
 	if (d == NULL)
-	d = malloc(sizeof(struct dog));
+		return;
 
-	d->name = name;
+
+	strncpy(d->name, name, sizeof(d->name) - 1);
+	d->name[sizeof(d->name) - 1] = '\0';
 	d->age = age;
-	d->owner = owner;
+	strncpy(d->owner, owner, sizeof(d->owner) - 1);
+	d->owner[sizeof(d->owner) - 1] = '\0';
 }
