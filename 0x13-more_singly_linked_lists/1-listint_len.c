7db2dadb91543557701a5cd55892bd99ed1d125f
MaryVeraUcheze <uchezemaryvera@gmail.com>
2023-04-03 16:29:44 -0700

    elements

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * listint_len - function that returns the number of elements in a linked listint_t list
+ * listint_len - returns the number of elements in a linked listint_t list
  * @h: pointer
  * Return: number of elements in the list
  */
