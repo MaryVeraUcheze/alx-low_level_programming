c5267546949b004cca8aac2539e63410a353002d
MaryVeraUcheze <uchezemaryvera@gmail.com>
2023-04-04 15:41:51 -0700

    backblock

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * add_nodeint_end - adds a new node at the end of a listint_t list
+ * @head: head pointer
+ * @n: integer to print
+ * Return: address of new elementvor NULL Iif fail
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *backblock, *temp;
+
+	backblock = malloc(sizeof(listint_t));
+
+	if (backblock == NULL)
+	return (NULL);
+
+	backblock->n = n;
+	backblock->next = NULL;
+
+	if (*head == NULL)
+	*head = backblock;
+	return (backblock);
+
+	temp = *head;
+	while (temp->next != NULL)
+	temp = temp->next;
+
+	temp->next = backblock;
+
+	return (backblock);
+}
