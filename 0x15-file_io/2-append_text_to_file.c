601d726e260bb0a7f846befbf5ab159645808351
MaryVeraUcheze <uchezemaryvera@gmail.com>
2023-04-13 04:30:26 -0700

    appendtext

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -8,22 +8,22 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-    int file, fwrite, v;
+	int file, fwrite, v;
 
-    if (filename == NULL)
-        return (-1);
-    file = open(filename, O_APPEND | O_RDWR, 0664);
-    if (file == -1)
-        return (-1);
+	if (filename == NULL)
+	return (-1);
+	file = open(filename, O_APPEND | O_RDWR, 0664);
+	if (file == -1)
+	return (-1);
 
-    if (text_content != NULL)
-    {
-        for (v = 0; text_content[v]; v++)
-            ;
-        fwrite = write(file, text_content, v);
-        if (fwrite == -1)
-            return (-1);
-    }
-    close(file);
-    return (1);
+	if (text_content != NULL)
+	{
+	for (v = 0; text_content[v]; v++)
+	;
+	fwrite = write(file, text_content, v);
+	if (fwrite == -1)
+	return (-1);
+}
+	close(file);
+	return (1);
 }
